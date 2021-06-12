82c3f008795bbd6f7b57c311c3793f1b0dfce087
GRENADEable <kahtim.dar@gmail.com>
2021-06-12 18:24:04 +0500

    - Setup ghost animation - Replaced one frame of player walk animations.

---
+++ TeamFrenchFries/Assets/Scripts/Player/GhostController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostController : MonoBehaviour
+{
+    #region Public Variables
+    [Space, Header("Data")]
+    public GameMangerData gmData;
+
+    [Space, Header("Ghost")]
+    public float followSpeed = 5f;
+    public float stoppingDistance = 0.5f;
+    public Transform followTarget;
+    #endregion
+
+    #region Private Variables
+    private float _distance;
+    private Vector2 _moveDirection;
+    private Animator _ghostAnim;
+    #endregion
+
+    #region Unity Callbacks
+    void Start()
+    {
+        _ghostAnim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        _distance = Vector3.Distance(transform.position, followTarget.position);
+
+        if (gmData.currState == GameMangerData.GameState.Game)
+        {
+            FollowPlayer();
+            PlayerInputs();
+        }
+        else
+        {
+            _ghostAnim.SetFloat("Horizontal", 0f);
+            _ghostAnim.SetFloat("Vertical", 0f);
+            _ghostAnim.SetFloat("Speed", 0f);
+        }
+    }
+    #endregion
+
+    #region My Functions
+    void FollowPlayer()
+    {
+        if (_distance > stoppingDistance)
+            transform.position = Vector2.MoveTowards(transform.position, followTarget.position, followSpeed * Time.fixedDeltaTime);
+    }
+
+    void PlayerInputs()
+    {
+        _moveDirection.x = Input.GetAxisRaw("Horizontal");
+        _moveDirection.y = Input.GetAxisRaw("Vertical");
+
+        _ghostAnim.SetFloat("Horizontal", _moveDirection.x);
+        _ghostAnim.SetFloat("Vertical", _moveDirection.y);
+        _moveDirection = _moveDirection.normalized;
+        _ghostAnim.SetFloat("Speed", _moveDirection.sqrMagnitude);
+    }
+    #endregion
+}
